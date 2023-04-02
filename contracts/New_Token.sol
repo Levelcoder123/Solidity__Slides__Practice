400a01990c5d8728a156b7046db7cbc9af79b4fd
Levelcoder123 <muaazkhan968@gmail.com>
2023-04-02 15:12:53 +0500

    Token_Added

--- contracts/New_Token.sol
+++ contracts/New_Token.sol
@@ -1,16 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract NDCoinERC20 {
+contract MyCoinERC20 {
 
+    // Transfer event must be triggered when tokens are transferred.
     event Transfer(address indexed from, address indexed to, uint tokens);
+
+    // Approval event must be triggered when spender is approved to collect tokens from owner.
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+    // Set the name of our token, its symbol, and the decimal to use.
 
     string public constant name = "My Coin";
     string public constant symbol = "MC";
-    uint8 public constant decimals = 18;
+    uint8 public constant decimals = 16;
 
-    mapping(address => uint256) balances;
+    // We will declare two mappings.
+
+    mapping(address => uint256) balances; // First for holding the token balance of each owner account.
 
     mapping(address => mapping (address => uint256)) allowed;
 
@@ -29,17 +36,17 @@ contract NDCoinERC20 {
         return balances[tokenOwner];
     }
 
- function transfer(address receiver, uint numTokens) public returns (bool) {
-        require(numTokens <= balances[msg.sender]);
-        balances[msg.sender] -= numTokens;
-        balances[receiver] += numTokens;
-        emit Transfer(msg.sender, receiver, numTokens);
+ function transfer(address receiver, uint numOfTokens) public returns (bool) {
+        require(numOfTokens <= balances[msg.sender]);
+        balances[msg.sender] -= numOfTokens;
+        balances[receiver] += numOfTokens;
+        emit Transfer(msg.sender, receiver, numOfTokens);
         return true;
     }
 
-    function approve(address delegate, uint numTokens) public returns (bool) {
-        allowed[msg.sender][delegate] = numTokens;
-        emit Approval(msg.sender, delegate, numTokens);
+    function approve(address delegate, uint numOfTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numOfTokens;
+        emit Approval(msg.sender, delegate, numOfTokens);
         return true;
     }
 
